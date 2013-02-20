f7906bfe2232ab746f12287dbc062ccb8a785ec8
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-02-20 07:28:51 -0800

    Switched to ErrorT internally

--- Control/Proxy/Parse.hs
+++ Control/Proxy/Parse.hs
@@ -11,14 +11,14 @@ module Control.Proxy.Parse (
 
 import Control.Applicative (Applicative(pure, (<*>)), Alternative(empty, (<|>)))
 import Control.Monad (MonadPlus(mzero, mplus))
-import Control.Monad.Trans (MonadTrans(lift))
+import Control.Monad.Trans.Class (MonadTrans(lift))
 import Control.Monad.Trans.State.Strict (StateT(StateT, runStateT))
-import Control.Monad.Trans.Either (EitherT(EitherT, runEitherT))
+import Control.Monad.Trans.Error (ErrorT(ErrorT, runErrorT))
 import qualified Control.Proxy as P
 import Control.Proxy ((>>~), (//>))
 import Control.Proxy.Trans.Maybe (MaybeP, nothing)
 import Control.Proxy.Trans.State (StateP(StateP))
-import Data.Monoid (Monoid(mempty, mappend), (<>))
+import Data.Monoid (Monoid(mempty), (<>))
 
 {- Use 'ParseT' to:
 
@@ -33,12 +33,12 @@ import Data.Monoid (Monoid(mempty, mappend), (<>))
     * diagnose parse failures with a stream of informative error messages
 -}
 newtype ParseT (p :: * -> * -> * -> * -> (* -> *) -> * -> *) s m r =
-    ParseT { unParseT :: StateT s (EitherT String (P.RespondT p () s s m)) r }
-{-                              ^                                  ^ ^
-                                |                                  | |
-                Leftover input -+              Request more input -+ |
-                                                                     |
-                                                  Total input drawn -+  -}
+    ParseT { unParseT :: StateT s (ErrorT String (P.RespondT p () s s m)) r }
+{-                              ^                                 ^ ^
+                                |                                 | |
+                Leftover input -+             Request more input -+ |
+                                                                    |
+                                                 Total input drawn -+  -}
 
 instance (Monad m, P.Interact p) => Functor (ParseT p s m) where
     fmap f p = ParseT (fmap f (unParseT p))
@@ -55,13 +55,13 @@ instance (P.Interact p) => MonadTrans (ParseT p s) where
     lift m = ParseT (lift (lift (lift m)))
 
 instance (Monad m, P.Interact p, Monoid s) => Alternative (ParseT p s m) where
-    empty = ParseT (StateT (\_ -> EitherT (P.RespondT (
+    empty = ParseT (StateT (\_ -> ErrorT (P.RespondT (
         P.runIdentityP (return mempty) ))))
-    p1 <|> p2 = ParseT (StateT (\s -> EitherT (P.RespondT (P.runIdentityP (do
+    p1 <|> p2 = ParseT (StateT (\s -> ErrorT (P.RespondT (P.runIdentityP (do
         draw  <- P.IdentityP (
-            P.runRespondT (runEitherT (runStateT (unParseT p1)  s         )))
+            P.runRespondT (runErrorT (runStateT (unParseT p1)  s         )))
         draw' <- P.IdentityP (
-            P.runRespondT (runEitherT (runStateT (unParseT p2) (s <> draw))))
+            P.runRespondT (runErrorT (runStateT (unParseT p2) (s <> draw))))
         return (draw <> draw') )))))
 
 instance (Monad m, P.Interact p, Monoid s) => MonadPlus (ParseT p s m) where
@@ -69,7 +69,7 @@ instance (Monad m, P.Interact p, Monoid s) => MonadPlus (ParseT p s m) where
     mplus = (<|>)
 
 parseError :: (Monad m, P.Interact p) => String -> ParseT p s m r
-parseError str = ParseT (StateT (\_ -> EitherT (return (Left str))))
+parseError str = ParseT (StateT (\_ -> ErrorT (return (Left str))))
 
 parseDebug :: (Monad m, P.Interact p, Monoid s) => String -> ParseT p s m ()
 parseDebug str = parseError str <|> pure ()
@@ -78,7 +78,7 @@ runParseT
  :: (Monad m, P.Interact p, Monoid s)
  => ParseT p s m r -> () -> P.Pipe p s r m ()
 runParseT p () = P.runIdentityP (do
-    P.IdentityP (P.runRespondT (runEitherT (runStateT (unParseT p) mempty))) //>
+    P.IdentityP (P.runRespondT (runErrorT (runStateT (unParseT p) mempty))) //>
         \x -> do
             case x of
                 Left   _     -> return mempty
@@ -91,7 +91,7 @@ debugParseT
  :: (Monad m, P.Interact p, Monoid s)
  => ParseT p s m r -> () -> P.Pipe p s (Either String r) m ()
 debugParseT p () = P.runIdentityP (do
-    P.IdentityP (P.runRespondT (runEitherT (runStateT (unParseT p) mempty))) //>
+    P.IdentityP (P.runRespondT (runErrorT (runStateT (unParseT p) mempty))) //>
         \x -> do
             P.respond (case x of
                 Left   e     -> Left  e
@@ -103,7 +103,7 @@ commit
  :: (Monad m, P.Interact p, Monoid s)
  => ParseT p s m r -> () -> P.Pipe (StateP s (MaybeP p)) s String m r
 commit p () = StateP $ \s ->
-    (do P.liftP (P.runRespondT (runEitherT (runStateT (unParseT p) s)) //> \x ->
+    (do P.liftP (P.runRespondT (runErrorT (runStateT (unParseT p) s)) //> \x ->
             P.runIdentityP (do
                 P.respond x
                 return mempty ) )
