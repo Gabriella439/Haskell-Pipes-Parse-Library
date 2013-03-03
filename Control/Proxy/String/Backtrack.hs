57439076d99a9911a53bf02cbbffeb6cb9598d71
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-03-02 21:21:51 -0800

    Added String parsers and Data.Char re-export

--- Control/Proxy/String/Backtrack.hs
+++ Control/Proxy/String/Backtrack.hs
@@ -1,3 +1,10 @@
+{-| 'String'-based parsers
+
+    Note that 'String's are very inefficient, and I will release future separate
+    packages with 'ByteString' and 'Text' operations.  I only include these to
+    allow users to test simple parsing without requiring additional library
+    dependencies.
+-}
 module Control.Proxy.String.Backtrack (
     -- * Single-character parsers
     drawChar,
@@ -11,6 +18,14 @@ module Control.Proxy.String.Backtrack (
     drawCharWhile,
     skipCharWhile,
     drawCharAll,
+    skipCharAll,
+
+    -- * Pushback
+    unDrawChar,
+
+    -- * Fail-safe parsers
+    drawCharMay,
+    peekChar,
 
     -- * Exact matches
     char,
@@ -18,13 +33,36 @@ module Control.Proxy.String.Backtrack (
 
     -- * Groups
     word, 
-    line
+    line,
+
+    -- * Re-exports
+    module Data.Char
     ) where
 
 import Control.Monad
 import Control.Proxy
 import Control.Proxy.Parse.Backtrack
-import Data.Char
+import Data.Char (
+    isControl,
+    isSpace,
+    isLower,
+    isUpper,
+    isAlpha,
+    isAlphaNum,
+    isPrint,
+    isDigit,
+    isOctDigit,
+    isHexDigit,
+    isLetter,
+    isMark,
+    isNumber,
+    isPunctuation,
+    isSymbol,
+    isSeparator,
+    isAscii,
+    isLatin1,
+    isAsciiUpper,
+    isAsciiLower )
 
 -- | Request a single character
 drawChar :: (Monad m, ListT p) => ParseT p String m Char
@@ -110,7 +148,7 @@ drawCharWhile pred = go id
 
 {-| Skip as many consecutive characters satisfying a predicate as possible
 
-    Faster than 'drawWhile' if you don't need the input
+    Faster than 'drawCharWhile' if you don't need the input
 -}
 skipCharWhile
     :: (Monad m, ListT p) => (Char -> Bool) -> ParseT p String m ()
@@ -122,37 +160,67 @@ skipCharWhile pred = go
             Nothing  -> return ()
             Just str -> do
                 let suffix = dropWhile pred str
-                if (null suffix)
-                    then go
-                    else unDraw suffix
+                if (null suffix) then go else unDraw suffix
 
+-- | Request the rest of the input
 drawCharAll :: (Monad m, ListT p) => ParseT p String m String
-drawCharAll = go id
+drawCharAll = fmap concat drawAll
+
+{-| Skip the rest of the input
+
+    Faster than 'drawCharAll' if you don't need the input
+-}
+skipCharAll :: (Monad m, ListT p) => ParseT p String m ()
+skipCharAll = skipAll
+
+-- | Push back a single character into the leftover buffer
+unDrawChar :: (Monad m, ListT p) => Char -> ParseT p String m ()
+unDrawChar c = unDraw [c]
+
+-- | Request 'Just' one element or 'Nothing' if at end of input
+drawCharMay :: (Monad m, ListT p) => ParseT p String m (Maybe Char)
+drawCharMay = go
   where
-    go diffStr = do
+    go = do
         mstr <- drawMay
         case mstr of
-            Nothing  -> return (diffStr "")
-            Just str -> go (diffStr . (str ++))
+            Nothing  -> return Nothing
+            Just str -> case str of
+                ""   -> go
+                c:cs -> do
+                    unDraw cs
+                    return (Just c)
 
+-- | Look ahead one element without consuming it
+peekChar :: (Monad m, ListT p) => ParseT p String m (Maybe Char)
+peekChar = do
+    m <- drawCharMay
+    case m of
+        Nothing -> return m
+        Just c  -> do
+            unDrawChar c
+            return m
+
+-- | Match a specific 'Char'
 char :: (Monad m, ListT p) => Char -> ParseT p String m Char
 char c = do
     c' <- drawChar
     if (c == c') then return c' else empty
 
+-- | Match a specific 'String'
 string :: (Monad m, ListT p) => String -> ParseT p String m String
 string str = do
     str' <- drawCharN (length str)
     if (str == str') then return str else empty
 
+-- | Match a non-empty word, skipping any preceding whitespace
 word :: (Monad m, ListT p) => ParseT p String m String
 word = do
     skipCharWhile isSpace
     str <- drawCharWhile (not . isSpace)
-    if (null str)
-        then empty
-        else return str
+    if (null str) then empty else return str
 
+-- | Match a line, consuming but not including the newline token
 line :: (Monad m, ListT p) => ParseT p String m String
 line = do
     str <- drawCharWhile (/= '\n')
