d83be7bc993ac39dd8f1bb2a9b931da692ff168e
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-03-03 21:44:48 -0800

    Removed manual namespacing of String parsers and added more re-exports

--- Control/Proxy/String/Backtrack.hs
+++ Control/Proxy/String/Backtrack.hs
@@ -7,25 +7,25 @@
 -}
 module Control.Proxy.String.Backtrack (
     -- * Single-character parsers
-    drawChar,
-    skipChar,
-    drawCharIf,
-    skipCharIf,
+    draw,
+    skip,
+    drawIf,
+    skipIf,
 
     -- * Efficient bulk parsers
-    drawCharN,
-    skipCharN,
-    drawCharWhile,
-    skipCharWhile,
-    drawCharAll,
-    skipCharAll,
+    drawN,
+    skipN,
+    drawWhile,
+    skipWhile,
+    drawAll,
+    skipAll,
 
     -- * Pushback
-    unDrawChar,
+    unDraw,
 
     -- * Fail-safe parsers
-    drawCharMay,
-    peekChar,
+    drawMay,
+    peek,
 
     -- * Exact matches
     char,
@@ -35,14 +35,48 @@ module Control.Proxy.String.Backtrack (
     word, 
     line,
 
+    -- * Numeric parsers
+    decimal,
+    signed,
+
+    -- * End of input
+    PB.endOfInput,
+    PB.isEndOfInput,
+
+    -- * Non-backtracking parsing
+    PB.commit,
+
+    -- * Run functions
+    PB.evalParseT,
+
+    -- * End-of-input utilities
+    PB.only,
+    PB.onlyK,
+    PB.just,
+    PB.justK,
+
     -- * Re-exports
     -- $reexport
-    module Data.Char
+    module Control.Applicative,
+    module Control.Monad,
+    module Data.Char,
+
+    PB.few,
+    PB.anything
     ) where
 
-import Control.Monad
+import Control.Applicative (
+    Applicative(pure, (<*>), (<*), (*>)),
+    Alternative(empty, (<|>), some, many),
+    (<$>),
+    (<$),
+    (<**>),
+    optional )
+import Control.Monad (
+    replicateM, replicateM_, MonadPlus(mzero, mplus), msum, mfilter, guard )
+import qualified Control.Monad as M
 import Control.Proxy
-import Control.Proxy.Parse.Backtrack
+import qualified Control.Proxy.Parse.Backtrack as PB
 import Data.Char (
     isControl,
     isSpace,
@@ -68,36 +102,36 @@ import qualified Data.Char as C
 import Data.List (foldl')
 
 -- | Request a single character
-drawChar :: (Monad m, ListT p) => ParseT p String m Char
-drawChar = do
-    skipWhile null
-    c:str <- draw
-    unDraw str
+draw :: (Monad m, ListT p) => PB.ParseT p String m Char
+draw = do
+    PB.skipWhile null
+    c:str <- PB.draw
+    PB.unDraw str
     return c
 
 -- | Skip a single character
-skipChar :: (Monad m, ListT p) => ParseT p String m ()
-skipChar = void drawChar
+skip :: (Monad m, ListT p) => PB.ParseT p String m ()
+skip = M.void draw
 
 -- | Request a single character that must satisfy the predicate
-drawCharIf :: (Monad m, ListT p) => (Char -> Bool) -> ParseT p String m Char
-drawCharIf pred = do
-    c <- drawChar
+drawIf :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m Char
+drawIf pred = do
+    c <- draw
     if (pred c) then return c else empty
 
 -- | Skip a single character that must satisfy the predicate
-skipCharIf :: (Monad m, ListT p) => (Char -> Bool) -> ParseT p String m ()
-skipCharIf pred = void (drawCharIf pred)
+skipIf :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
+skipIf pred = M.void (drawIf pred)
 
 -- | Request a fixed number of characters
-drawCharN :: (Monad m, ListT p) => Int -> ParseT p String m String
-drawCharN = go0
+drawN :: (Monad m, ListT p) => Int -> PB.ParseT p String m String
+drawN = go0
   where
     go0 n = if (n <= 0)
         then return ""
         else go1 id n
     go1 diffStr n = do
-        mstr <- drawMay
+        mstr <- PB.drawMay
         case mstr of
             Nothing  -> empty
             Just str -> do
@@ -106,21 +140,21 @@ drawCharN = go0
                     then go1 (diffStr . (str ++)) (n - len)
                     else do
                         let (prefix, suffix) = splitAt n str
-                        unDraw suffix
+                        PB.unDraw suffix
                         return (diffStr prefix)
 
 {-| Skip a fixed number of characters
 
-    Faster than 'drawCharN' if you don't need the input
+    Faster than 'drawN' if you don't need the input
 -}
-skipCharN :: (Monad m, ListT p) => Int -> ParseT p String m ()
-skipCharN = go0
+skipN :: (Monad m, ListT p) => Int -> PB.ParseT p String m ()
+skipN = go0
   where
     go0 n = if (n <= 0)
         then return ()
         else go1 n
     go1 n = do
-        mstr <- drawMay
+        mstr <- PB.drawMay
         case mstr of
             Nothing  -> empty
             Just str -> do
@@ -129,16 +163,16 @@ skipCharN = go0
                     then go1 (n - len)
                     else do
                         let suffix = drop n str
-                        unDraw suffix
+                        PB.unDraw suffix
                         return ()
 
 -- | Request as many consecutive characters satisfying a predicate as possible
-drawCharWhile
-    :: (Monad m, ListT p) => (Char -> Bool) -> ParseT p String m String
-drawCharWhile pred = go id
+drawWhile
+    :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m String
+drawWhile pred = go id
   where
     go diffStr = do
-        mstr <- drawMay
+        mstr <- PB.drawMay
         case mstr of
             Nothing  -> return (diffStr "")
             Just str -> do
@@ -146,100 +180,103 @@ drawCharWhile pred = go id
                 if (null suffix)
                     then go (diffStr . (prefix ++))
                     else do
-                        unDraw suffix
+                        PB.unDraw suffix
                         return (diffStr prefix)
 
 {-| Skip as many consecutive characters satisfying a predicate as possible
 
-    Faster than 'drawCharWhile' if you don't need the input
+    Faster than 'drawWhile' if you don't need the input
 -}
-skipCharWhile
-    :: (Monad m, ListT p) => (Char -> Bool) -> ParseT p String m ()
-skipCharWhile pred = go
+skipWhile
+    :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
+skipWhile pred = go
   where
     go = do
-        mstr <- drawMay
+        mstr <- PB.drawMay
         case mstr of
             Nothing  -> return ()
             Just str -> do
                 let suffix = dropWhile pred str
-                if (null suffix) then go else unDraw suffix
+                if (null suffix) then go else PB.unDraw suffix
 
 -- | Request the rest of the input
-drawCharAll :: (Monad m, ListT p) => ParseT p String m String
-drawCharAll = concat <$> drawAll
+drawAll :: (Monad m, ListT p) => PB.ParseT p String m String
+drawAll = concat <$> PB.drawAll
 
 {-| Skip the rest of the input
 
-    Faster than 'drawCharAll' if you don't need the input
+    Faster than 'drawAll' if you don't need the input
 -}
-skipCharAll :: (Monad m, ListT p) => ParseT p String m ()
-skipCharAll = skipAll
+skipAll :: (Monad m, ListT p) => PB.ParseT p String m ()
+skipAll = PB.skipAll
 
 -- | Push back a single character into the leftover buffer
-unDrawChar :: (Monad m, ListT p) => Char -> ParseT p String m ()
-unDrawChar c = unDraw [c]
+unDraw :: (Monad m, ListT p) => Char -> PB.ParseT p String m ()
+unDraw c = PB.unDraw [c]
 
 -- | Request 'Just' one element or 'Nothing' if at end of input
-drawCharMay :: (Monad m, ListT p) => ParseT p String m (Maybe Char)
-drawCharMay = go
+drawMay :: (Monad m, ListT p) => PB.ParseT p String m (Maybe Char)
+drawMay = go
   where
     go = do
-        mstr <- drawMay
+        mstr <- PB.drawMay
         case mstr of
             Nothing  -> return Nothing
             Just str -> case str of
                 ""   -> go
                 c:cs -> do
-                    unDraw cs
+                    PB.unDraw cs
                     return (Just c)
 
 -- | Look ahead one element without consuming it
-peekChar :: (Monad m, ListT p) => ParseT p String m (Maybe Char)
-peekChar = do
-    m <- drawCharMay
+peek :: (Monad m, ListT p) => PB.ParseT p String m (Maybe Char)
+peek = do
+    m <- drawMay
     case m of
         Nothing -> return m
         Just c  -> do
-            unDrawChar c
+            unDraw c
             return m
 
 -- | Match a specific 'Char'
-char :: (Monad m, ListT p) => Char -> ParseT p String m Char
+char :: (Monad m, ListT p) => Char -> PB.ParseT p String m Char
 char c = do
-    c' <- drawChar
+    c' <- draw
     if (c == c') then return c' else empty
 
 -- | Match a specific 'String'
-string :: (Monad m, ListT p) => String -> ParseT p String m String
+string :: (Monad m, ListT p) => String -> PB.ParseT p String m String
 string str = do
-    str' <- drawCharN (length str)
+    str' <- drawN (length str)
     if (str == str') then return str else empty
 
 -- | Match a non-empty word, skipping any preceding whitespace
-word :: (Monad m, ListT p) => ParseT p String m String
+word :: (Monad m, ListT p) => PB.ParseT p String m String
 word = do
-    skipCharWhile isSpace
-    str <- drawCharWhile (not . isSpace)
+    skipWhile isSpace
+    str <- drawWhile (not . isSpace)
     if (null str) then empty else return str
 
 -- | Match a line, consuming but not including the newline token
-line :: (Monad m, ListT p) => ParseT p String m String
+line :: (Monad m, ListT p) => PB.ParseT p String m String
 line = do
-    str <- drawCharWhile (/= '\n')
+    str <- drawWhile (/= '\n')
     char '\n'
     return str
 
-decimal :: (Monad m, ListT p, Integral r) => ParseT p String m r
+-- | Parse an unsigned 'Integral' number
+decimal :: (Monad m, ListT p, Integral r) => PB.ParseT p String m r
 decimal = do
-    digits <- drawCharWhile (\c -> c >= '0' && c <= '9')
+    digits <- drawWhile (\c -> c >= '0' && c <= '9')
     if (null digits)
         then empty
         else return $
                  foldl' (\n c -> n * 10 + fromIntegral (C.ord c - 48)) 0 digits
 
+-- | Convert a an unsigned number parser to a signed number parser
 signed
-    :: (Monad m, ListT p, Num r) => ParseT p String m r -> ParseT p String m r
+    :: (Monad m, ListT p, Num r)
+    => PB.ParseT p String m r -> PB.ParseT p String m r
 signed p = msum
     [ negate <$> (char '-' *> p)
     , char '+' *> p
@@ -247,5 +284,11 @@ signed p = msum
     ]
 
 {- $reexport
-    The @Data.Char@ re-export includes all the 'isXXX' classification functions.
+    @Control.Applicative@ exports useful combinators for 'Functor',
+    'Applicative', and 'Alternative', like 'many', ('<|>'), and 'optional'.
+
+    @Control.Monad@ exports useful combinators for 'Monad' and 'MonadPlus',
+    like 'replicateM', 'msum', and 'mfilter'.
+
+    @Data.Char@ exports all the 'isXXX' character classification functions.
 -}
