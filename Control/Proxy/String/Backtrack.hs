68c0c6630a9fa1247af180822e9bd63370aa131e
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-03-03 23:50:45 -0800

    Added delimited parsers for Strings and qualified imports

--- Control/Proxy/String/Backtrack.hs
+++ Control/Proxy/String/Backtrack.hs
@@ -1,4 +1,4 @@
-{-| 'String'-based parsers
+{-| Backtracking parsers for 'String's
 
     Note that 'String's are very inefficient, and I will release future separate
     packages with 'ByteString' and 'Text' operations.  I only include these to
@@ -6,6 +6,9 @@
     dependencies.
 -}
 module Control.Proxy.String.Backtrack (
+    -- * Backtracking parsers
+    ParseT,
+
     -- * Single-character parsers
     draw,
     skip,
@@ -27,6 +30,10 @@ module Control.Proxy.String.Backtrack (
     drawMay,
     peek,
 
+    -- * Delimited parsing
+    parseN,
+    parseWhile,
+
     -- * Exact matches
     char,
     string,
@@ -40,8 +47,9 @@ module Control.Proxy.String.Backtrack (
     signed,
 
     -- * End of input
-    PB.endOfInput,
-    PB.isEndOfInput,
+    -- $eof
+    endOfInput,
+    isEndOfInput,
 
     -- * Non-backtracking parsing
     PB.commit,
@@ -75,8 +83,10 @@ import Control.Applicative (
 import Control.Monad (
     replicateM, replicateM_, MonadPlus(mzero, mplus), msum, mfilter, guard )
 import qualified Control.Monad as M
-import Control.Proxy
+import Control.Monad.Trans.State.Strict (StateT(StateT))
+import qualified Control.Proxy as P
 import qualified Control.Proxy.Parse.Backtrack as PB
+import Control.Proxy.Parse.Internal (ParseT(ParseT))
 import Data.Char (
     isControl,
     isSpace,
@@ -100,9 +110,12 @@ import Data.Char (
     isAsciiLower )
 import qualified Data.Char as C
 import Data.List (foldl')
+import Data.Maybe (isJust)
+import qualified Data.Sequence as S
+import Data.Sequence (ViewL((:<)), (<|), (|>), (><))
 
 -- | Request a single character
-draw :: (Monad m, ListT p) => PB.ParseT p String m Char
+draw :: (Monad m, P.ListT p) => PB.ParseT p String m Char
 draw = do
     PB.skipWhile null
     c:str <- PB.draw
@@ -110,21 +123,21 @@ draw = do
     return c
 
 -- | Skip a single character
-skip :: (Monad m, ListT p) => PB.ParseT p String m ()
+skip :: (Monad m, P.ListT p) => PB.ParseT p String m ()
 skip = M.void draw
 
 -- | Request a single character that must satisfy the predicate
-drawIf :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m Char
+drawIf :: (Monad m, P.ListT p) => (Char -> Bool) -> PB.ParseT p String m Char
 drawIf pred = do
     c <- draw
     if (pred c) then return c else empty
 
 -- | Skip a single character that must satisfy the predicate
-skipIf :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
+skipIf :: (Monad m, P.ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
 skipIf pred = M.void (drawIf pred)
 
 -- | Request a fixed number of characters
-drawN :: (Monad m, ListT p) => Int -> PB.ParseT p String m String
+drawN :: (Monad m, P.ListT p) => Int -> PB.ParseT p String m String
 drawN = go0
   where
     go0 n = if (n <= 0)
@@ -147,7 +160,7 @@ drawN = go0
 
     Faster than 'drawN' if you don't need the input
 -}
-skipN :: (Monad m, ListT p) => Int -> PB.ParseT p String m ()
+skipN :: (Monad m, P.ListT p) => Int -> PB.ParseT p String m ()
 skipN = go0
   where
     go0 n = if (n <= 0)
@@ -168,7 +181,7 @@ skipN = go0
 
 -- | Request as many consecutive characters satisfying a predicate as possible
 drawWhile
-    :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m String
+    :: (Monad m, P.ListT p) => (Char -> Bool) -> PB.ParseT p String m String
 drawWhile pred = go id
   where
     go diffStr = do
@@ -188,7 +201,7 @@ drawWhile pred = go id
     Faster than 'drawWhile' if you don't need the input
 -}
 skipWhile
-    :: (Monad m, ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
+    :: (Monad m, P.ListT p) => (Char -> Bool) -> PB.ParseT p String m ()
 skipWhile pred = go
   where
     go = do
@@ -200,22 +213,22 @@ skipWhile pred = go
                 if (null suffix) then go else PB.unDraw suffix
 
 -- | Request the rest of the input
-drawAll :: (Monad m, ListT p) => PB.ParseT p String m String
+drawAll :: (Monad m, P.ListT p) => PB.ParseT p String m String
 drawAll = concat <$> PB.drawAll
 
 {-| Skip the rest of the input
 
     Faster than 'drawAll' if you don't need the input
 -}
-skipAll :: (Monad m, ListT p) => PB.ParseT p String m ()
+skipAll :: (Monad m, P.ListT p) => PB.ParseT p String m ()
 skipAll = PB.skipAll
 
 -- | Push back a single character into the leftover buffer
-unDraw :: (Monad m, ListT p) => Char -> PB.ParseT p String m ()
+unDraw :: (Monad m, P.ListT p) => Char -> PB.ParseT p String m ()
 unDraw c = PB.unDraw [c]
 
 -- | Request 'Just' one element or 'Nothing' if at end of input
-drawMay :: (Monad m, ListT p) => PB.ParseT p String m (Maybe Char)
+drawMay :: (Monad m, P.ListT p) => PB.ParseT p String m (Maybe Char)
 drawMay = go
   where
     go = do
@@ -229,7 +242,7 @@ drawMay = go
                     return (Just c)
 
 -- | Look ahead one element without consuming it
-peek :: (Monad m, ListT p) => PB.ParseT p String m (Maybe Char)
+peek :: (Monad m, P.ListT p) => PB.ParseT p String m (Maybe Char)
 peek = do
     m <- drawMay
     case m of
@@ -238,34 +251,126 @@ peek = do
             unDraw c
             return m
 
+{- NOTE: parseN and parseWhile work by inserting a fake end-of-input marker
+         (i.e. Nothing) using a 'block' function and then removing it after the
+         parser completes using an 'unblock' function.  This trick does not
+         work if the user's parser can consume the end-of-input marker, so I
+         cannot expose any parsing primitives that consume this marker.
+-}
+
+{-| @(parseN n p)@ restricts the parser @p@ to only use the next @n@ characters.
+-}
+parseN
+    :: (Monad m, P.ListT p) => Int -> ParseT p String m r -> ParseT p String m r
+parseN n0 p = do
+    block
+    r <- p
+    unBlock
+    return r
+  where
+    block = ParseT (StateT (\s -> P.RespondT (go0 S.empty s n0)))
+    go0 s1 s2 n = if (n > 0)
+        then case S.viewl s2 of
+            S.EmptyL -> go1 s1 n
+            ma:<mas  -> case ma of
+                Nothing -> return S.empty
+                Just a -> do
+                    let len = length a
+                    if (n > len)
+                        then go0 (s1 |> ma) mas $! n - len
+                        else do
+                            let (prefix, suffix) = splitAt n a
+                            P.respond ((),
+                                (s1 |> Just prefix |> Nothing)
+                             >< (Just suffix <| mas) )
+        else P.respond ((), (s1 |> Nothing) >< s2)
+    go1 s n = if (n > 0)
+        then do
+            ma <- P.request ()
+            case ma of
+                Nothing -> return (S.singleton ma)
+                Just a  -> fmap (ma <|) (do
+                    let len = length a
+                    if (n > len)
+                        then go1 (s |> ma) $! n - len
+                        else do
+                            let (prefix, suffix) = splitAt n a
+                            P.respond ((),
+                                s |> Just prefix |> Nothing |> Just suffix ) )
+        else P.respond ((), s |> Nothing)
+    unBlock = ParseT (StateT (\s -> P.RespondT (do
+        let (prefix, suffix) = S.spanl isJust s
+        P.respond ((), prefix >< (case S.viewl suffix of
+            S.EmptyL -> suffix
+            _:<mas   -> mas )) )))
+
+{-| @(parseWhile pred p)@ restricts the parser @p@ to only use characters that
+    satisfies the predicate @pred@. -}
+parseWhile
+    :: (Monad m, P.ListT p)
+    => (Char -> Bool) -> ParseT p String m r -> ParseT p String m r
+parseWhile pred p = do
+    block
+    r <- p
+    unBlock
+    return r
+  where
+    block = ParseT (StateT (\s -> P.RespondT (go0 S.empty s)))
+    go0 s1 s2 = case S.viewl s2 of
+        S.EmptyL -> go1 s1
+        ma:<mas  -> case ma of
+            Nothing -> P.respond ((), (s1 |> Nothing) >< s2)
+            Just a -> do
+                let (prefix, suffix) = span pred a
+                if (null suffix)
+                    then go0 (s1 |> ma) mas
+                    else P.respond ((),
+                            (s1 |> Just prefix |> Nothing)
+                         >< (Just suffix <| mas) )
+    go1 s = do
+        ma <- P.request ()
+        fmap (ma <|) (case ma of
+            Nothing -> P.respond ((), s |> Nothing |> ma)
+            Just a  -> do
+                let (prefix, suffix) = span pred a
+                if (null suffix)
+                    then go1 (s |> ma)
+                    else P.respond ((),
+                            s |> Just prefix |> Nothing |> Just suffix ) )
+    unBlock = ParseT (StateT (\s -> P.RespondT (do
+        let (prefix, suffix) = S.spanl isJust s
+        P.respond ((), prefix >< (case S.viewl suffix of
+            S.EmptyL -> suffix
+            _:<mas   -> mas )) )))
+
 -- | Match a specific 'Char'
-char :: (Monad m, ListT p) => Char -> PB.ParseT p String m Char
+char :: (Monad m, P.ListT p) => Char -> PB.ParseT p String m Char
 char c = do
     c' <- draw
     if (c == c') then return c' else empty
 
 -- | Match a specific 'String'
-string :: (Monad m, ListT p) => String -> PB.ParseT p String m String
+string :: (Monad m, P.ListT p) => String -> PB.ParseT p String m String
 string str = do
     str' <- drawN (length str)
     if (str == str') then return str else empty
 
 -- | Match a non-empty word, skipping any preceding whitespace
-word :: (Monad m, ListT p) => PB.ParseT p String m String
+word :: (Monad m, P.ListT p) => PB.ParseT p String m String
 word = do
     skipWhile isSpace
     str <- drawWhile (not . isSpace)
     if (null str) then empty else return str
 
 -- | Match a line, consuming but not including the newline token
-line :: (Monad m, ListT p) => PB.ParseT p String m String
+line :: (Monad m, P.ListT p) => PB.ParseT p String m String
 line = do
     str <- drawWhile (/= '\n')
     char '\n'
     return str
 
 -- | Parse an unsigned 'Integral' number
-decimal :: (Monad m, ListT p, Integral r) => PB.ParseT p String m r
+decimal :: (Monad m, P.ListT p, Integral r) => PB.ParseT p String m r
 decimal = do
     digits <- drawWhile (\c -> c >= '0' && c <= '9')
     if (null digits)
@@ -273,9 +378,28 @@ decimal = do
         else return $
                  foldl' (\n c -> n * 10 + fromIntegral (C.ord c - 48)) 0 digits
 
+{- $eof
+    Note that these differ from the end-of-input parsers in
+    "Control.Proxy.Parse.Backtrack".  These parsers will ignore chunks of empty
+    'String's when searching for the end-of-input marker since they only care
+    about whether the 'String' is complete.
+-}
+
+-- | Match end of 'String' without consuming it
+endOfInput :: (Monad m, P.ListT p) => PB.ParseT p String m ()
+endOfInput = do
+    PB.skipWhile null
+    PB.endOfInput
+
+-- | Return whether cursor is at end of 'String'
+isEndOfInput :: (Monad m, P.ListT p) => PB.ParseT p String m Bool
+isEndOfInput = do
+    PB.skipWhile null
+    PB.isEndOfInput
+
 -- | Convert a an unsigned number parser to a signed number parser
 signed
-    :: (Monad m, ListT p, Num r)
+    :: (Monad m, P.ListT p, Num r)
     => PB.ParseT p String m r -> PB.ParseT p String m r
 signed p = msum
     [ negate <$> (char '-' *> p)
