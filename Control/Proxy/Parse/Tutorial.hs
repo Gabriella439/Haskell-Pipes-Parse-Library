e3039814ca00aa8b5709a50b5e96bbb77b6021cc
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-05-28 23:31:55 -0700

    Updated documentation

--- Control/Proxy/Parse/Tutorial.hs
+++ Control/Proxy/Parse/Tutorial.hs
@@ -1,3 +1,9 @@
+{-| This module provides a tutorial for the @pipes-concurrency@ library
+
+    This tutorial assumes that you have read the @pipes@ tutorial in
+    @Control.Proxy.Tutorial@.
+-}
+
 module Control.Proxy.Parse.Tutorial (
     -- * Introduction
     -- $introduction
@@ -11,20 +17,29 @@ module Control.Proxy.Parse.Tutorial (
     -- * Pushback and leftovers
     -- $leftovers
 
-    -- * Mixing leftovers
+    -- * Isolating leftovers
+    -- $mix
+
+    -- * Diverse leftovers
+    -- $diverse
+
+    -- * Lenses
     -- $lenses
     ) where
+
+import Control.Proxy
+import Control.Proxy.Parse
 
 {- $introduction
     @pipes-parse@ provides utilities commonly required for parsing streams:
 
-    * End of input utilities and conventions
+    * End of input utilities and conventions for the @pipes-ecosystem@
 
     * Pushback and leftovers support for saving unused input
 
-    * Tools to combine parsing stages that share leftover buffers
-
     * Tools to combine parsing stages with diverse or isolated leftover buffers
+
+    Use these utilities to parse and validate input in constant memory.
 -}
 
 {- $eof
@@ -56,8 +71,8 @@ Nothing
 
 > unwrap :: (Monad m, Proxy p) => x -> p x (Maybe a) x a m ()
 
-    Compose 'unwrap' downstream to unwrap every 'Just' and terminate on the
-    first 'Nothing':
+    Compose 'unwrap' downstream of a pipe to unwrap every 'Just' and terminate
+    on the first 'Nothing':
 
 >>> runProxy $ wrap . enumFromToS 1 3 >-> printD >-> unwrap
 Just 1
@@ -133,11 +148,166 @@ Just 99
 
 -}
 
-{- $lenses
-    Why use 'mempty' instead of '[]'?  @pipes-parse@ lets you easily mix
-    multiple distinct leftovers buffer into the same 'StateP' layer and 'mempty'
-    will still do the correct thing when you start combining buffers.
+{- $mix
+    Why use 'mempty' instead of @[]@?  @pipes-parse@ lets you easily mix
+    distinct leftovers buffers into the same 'StateP' layer and 'mempty' will
+    still do the correct thing when you use multiple buffers.
 
-    For example, imagine that you want to compose two separate parsers, one of
-    
+    For example, let's say that we want to mix two of the @pipes-parse@
+    utilities, 'passUpTo' and 'drawAll':
+
+> -- Transmit up to the specified number of elements
+> passUpTo
+>     :: (Monad m, Proxy p) 
+>     => Int -> () -> Pipe (StateP [a] p) (Maybe a) (Maybe a) m r
+>
+> -- Fold all input into a list
+> drawAll :: (Monad m, Proxy p) => () -> StateP [a] p () (Maybe a) y' y m [a]
+
+    We might expect this to yield chunks of three elements at a time:
+
+> chunks :: (Monad m, Proxy p) => () -> Pipe (StateP [a] p) a [a] m ()
+> chunks () = loop
+>   where
+>     loop = do
+>         as <- (passUpTo 3 >-> drawAll) ()
+>         respond as
+>         eof <- isEndOfInput
+>         unless eof loop
+
+    ... but it doesn't:
+
+>>> runProxy $ evalStateK mempty $ wrap . enumFromToS 1 15 >-> chunks >-> printD
+[1,2,3]
+[4,5,6,7]
+[8,9,10,11]
+[12,13,14,15]
+
+    @chunks@ behaves strangely because both 'drawAll' shares the same leftovers
+    buffer as 'passUpTo' and 'isEndOfInput'.  After the first chunk completes,
+    'isEndOfInput' peeks at the next value, @4@, and immediately 'unDraw's the
+    value.  'drawAll' retrieves this value before consulting 'passUpTo' which is
+    why every subsequent list contains an extra element.
+
+    We often don't want composed parsing stages to share the same leftovers
+    buffer, and @pipes-parse@ provides a way to reflect that in the types.  We
+    can change the type of @chunks@ to:
+
+> chunks :: (Monad m, Proxy p) => () -> Pipe (StateP ([a], [a]) p) a [a] m ()
+
+    Now our 'StateP' layer holds two separate leftovers buffers, and we can
+    specify which leftovers buffer each parsing primitive should interact with
+    using lenses:
+
+> chunks () = loop
+>   where
+>     loop = do
+>         as  <- (zoom _fst . passUpTo 3 >-> zoom _snd . drawAll) ()
+>         respond as
+>         eof <- zoom _fst isEndOfInput
+>         unless eof loop
+
+    We use @zoom _fst@ to target both `passUpTo` and `isEndOfInput` to the first
+    leftovers buffer and @zoom _snd@ to target `drawAll` to the second leftovers
+    buffer.  This isolates the two leftovers buffers from each other, giving the
+    desired behavior:
+
+>>> runProxy $ evalStateK mempty $ wrap . enumFromToS 1 15 >-> chunks >-> printD
+[1,2,3]
+[4,5,6]
+[7,8,9]
+[10,11,12]
+[13,14,15]
+
+    Notice that 'mempty' still initializes both leftovers buffers correctly
+    because:
+
+> (mempty :: ([a], [a])) = ([], [])
+-}
+
+{- $diverse
+    This trick is also useful for mixing stages with different leftover types,
+    such as the following two pipes:
+
+> adder
+>     :: (Monad m, Proxy p) => () -> Consumer (StateP [Int] p) (Maybe Int) m Int
+> adder () = fmap sum $ drawAll ()
+> 
+> tallyLength
+>     :: (Monad m, Proxy p)
+>     => () -> Pipe (StateP [String] p) (Maybe String) (Maybe Int) m r
+> tallyLength () = loop 0
+>   where
+>     loop tally = do
+>         respond (Just tally)
+>         mstr <- draw
+>         case mstr of
+>             Nothing  -> forever $ respond Nothing
+>             Just str -> loop (tally + length str)
+
+    We can use the same 'zoom' trick to unify them into a single 'StateP' layer:
+
+> combined
+>     :: (Monad m, Proxy p)
+>     => () -> Consumer (StateP ([String], [Int]) p) (Maybe String) m Int
+> combined = zoom _fst . tallyLength >-> zoom _snd . adder
+>
+> source :: (Monad m, Proxy p) => () -> Producer p (Maybe String) m ()
+
+    ... which gives the correct behavior:
+
+>>> runProxy $ evalStateK mempty $ source >-> combined
+20
+
+-}
+
+{- $lenses
+    Let's study the type of 'zoom' to understand how it works:
+
+> -- zoom's true type is slightly different to avoid a dependency on `lens`
+> zoom :: Lens' s1 s2 -> StateP s2 p a' a b' b m r -> StateP s1 p a' a b' b m r
+
+    'zoom' behaves like the function of the same name from the @lens@ package,
+    zooming in on a substate of a global state using the provided lens.  When we
+    give it the '_fst' lens we zoom in on the first element of a tuple:
+
+> _fst :: Lens' (a, b) a
+>
+> zoom _fst :: StateP l1 p a' a b' b m r -> StateP (l1, l2) p a' a b' b m r
+
+    ... and when we give it the '_snd' lens we zoom in on the second element of
+    a tuple:
+
+> _snd :: Lens' (a, b) b
+>
+> zoom _snd :: StateP l2 p a' a b' b m r -> StateP (l1, l2) p a' a b' b m r
+
+    '_fst' and '_snd' are like '_1' and '_2' from the @lens@ package, except
+    with a more monomorphic type.  This ensures that type inference works
+    correctly when supplying 'mempty' as the initial state.
+
+    If you want to merge more than one leftovers buffer, you can either nest
+    pairs of tuples:
+
+> p = zoom _fst . p1 >-> zoom (_snd . _fst) . p2 >-> zoom (_snd . _snd) . p3
+
+    ... or you can create a data type that holds all your leftovers and generate
+    lenses to its fields:
+
+> import Control.Lens hiding (zoom)
+>
+> data Leftovers = Leftovers
+>     { _buf1 :: [String]
+>     , _buf2 :: [Int]
+>     , _buf3 :: [Double]
+>     }
+> makeLenses ''Leftovers
+>
+> p = zoom _buf1 . p1 >-> zoom _buf2 . p2 >-> zoom _buf3 . p3
+
+    'zoom' will work with all lenses from the @lens@ package.
+
+    FIXME: Perhaps remove MonadState instance or move it into @pipes@ since it's
+    unnecessary for mixing leftovers buffers.
+    lefto
 -}
