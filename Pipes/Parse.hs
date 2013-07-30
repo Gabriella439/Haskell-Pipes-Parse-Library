e823997a255c085dcc44eccb5eb73bec3665bb99
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-07-30 15:44:57 -0700

    New lens-based parsing

--- Pipes/Parse.hs
+++ Pipes/Parse.hs
@@ -1,276 +1,143 @@
-{-# LANGUAGE RankNTypes #-}
-
 -- | Parsing utilities for pipes
 
 module Pipes.Parse (
-    -- * Pushback and Leftovers
-    -- $pushback
+    -- * Primitives
     draw,
     unDraw,
 
-    -- * Utilities
-    peek,
-    isEndOfInput,
-    drawAll,
-    skipAll,
-    passUpTo,
-    passWhile,
+    -- * Producer
+    input,
 
-    -- * Adapters
-    -- $adapters
-    wrap,
-    unwrap,
-    using,
-    fmapPull,
-    returnPull,
-    bindPull,
-
-    -- * Type Synonyms
-    Draw,
-    Sink,
-    Source,
-    Conduit,
+    -- * Isomorphisms
+    -- $isomorphisms
+    spans,
+    splits,
 
     -- * Re-exports
-    -- $reexports
-    module Control.Monad.Trans.State.Strict
+    -- $re-exports
+    module Control.Lens,
+    module Control.Monad.IO.Class,
+    module Control.Monad.Trans.State
     ) where
 
-import Control.Monad (forever)
+import Control.Lens (Iso', zoom)
+import qualified Control.Lens as L
+import Control.Monad (join)
+import Control.Monad.IO.Class (MonadIO(liftIO))
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State.Strict (
-    StateT(runStateT), evalStateT, execStateT, runState, evalState, execState )
-import qualified Control.Monad.Trans.State.Strict as S
-import Pipes
-import Pipes.Lift
-import qualified Pipes.Prelude as P
+import Control.Monad.Trans.State (StateT, runStateT, evalStateT, execStateT)
+import qualified Control.Monad.Trans.State as S
+import Pipes (Producer, yield, next)
+import Pipes.Core (Producer')
+import Prelude hiding (splitAt, span)
 
-{- $pushback
-    'unDraw' stores all leftovers in a 'StateP' buffer and 'draw' retrieves
-    leftovers in last-in-first-out (LIFO) order from this buffer before drawing
-    new input from upstream.
--}
-
-{-| Like @request ()@, except try to use the leftovers buffer first
-
-    A 'Nothing' return value indicates end of input.
--}
-draw :: (Monad m) => Sink a (StateT [a] m) (Maybe a)
+-- | Draw a single element from the 'Producer'
+draw :: (Monad m) => StateT (Producer a m r) m (Maybe a)
 draw = do
-    s <- lift S.get
-    case s of
-        []   -> request Draw
-        a:as -> do
-            lift $ S.put as
-            return (Just a)
-{-# INLINABLE draw #-}
+    p <- S.get
+    x <- lift (next p)
+    case x of
+        Left   _      -> return Nothing
+        Right (e, p') -> do
+            S.put p'
+            return (Just e)
 
--- | Push an element back onto the leftovers buffer
-unDraw :: (Monad m) => a -> Effect (StateT [a] m) ()
-unDraw a = lift $ S.modify (a:)
-{-# INLINABLE unDraw #-}
+-- | Push a single element back onto the 'Producer'
+unDraw :: (Monad m) => a -> StateT (Producer a m r) m ()
+unDraw e = S.modify (yield e >>)
 
--- | Peek at the next element without consuming it
-peek :: (Monad m) => Sink a (StateT [a] m) (Maybe a)
-peek = do
-    ma <- draw
-    case ma of
-        Nothing -> return ()
-        Just a  -> unDraw a
-    return ma
-{-# INLINABLE peek #-}
+{-| Stream from the stored 'Producer'
 
--- | Check if at end of input stream.
-isEndOfInput :: (Monad m) => Sink a (StateT [a] m) Bool
-isEndOfInput = do
-    ma <- peek
-    case ma of
-        Nothing -> return True
-        Just _  -> return False
-{-# INLINABLE isEndOfInput #-}
-
-{-| Fold all input into a list
-
-    Note: 'drawAll' is usually an anti-pattern.
+    Streaming from 'input' differs from streaming directly from the underlying
+    'Producer' because any unused input is saved for later.
 -}
-drawAll :: (Monad m) => () -> Sink a (StateT [a] m) [a]
-drawAll = \() -> go id
+input :: (Monad m) => Producer' a (StateT (Producer a m r) m) ()
+input = loop
   where
-    go diffAs = do
-        ma <- draw
-        case ma of
-            Nothing -> return (diffAs [])
-            Just a  -> go (diffAs . (a:))
-{-# INLINABLE drawAll #-}
-
--- | Consume the input completely, discarding all values
-skipAll :: (Monad m) => () -> Sink a (StateT [a] m) ()
-skipAll = \() -> go
-  where
-    go = do
-        ma <- draw
-        case ma of
+    loop = do
+        me <- lift draw
+        case me of
             Nothing -> return ()
-            Just _  -> go
-{-# INLINABLE skipAll #-}
+            Just e  -> do
+                yield e
+                loop
 
--- | Forward up to the specified number of elements downstream
-passUpTo :: (Monad m) => Int -> Draw -> Conduit a a (StateT [a] m) r
-passUpTo n0 = \_ -> go n0
-  where
-    go n0 =
-        if (n0 <= 0)
-        then forever $ respond Nothing
-        else do
-            ma <- draw
-            respond ma
-            case ma of
-                Nothing -> forever $ respond Nothing
-                Just _  -> go (n0 - 1)
-{-# INLINABLE passUpTo #-}
+{- $isomorphisms
+    Use these isomorphisms with 'zoom' to limit a parser to a subset of the
+    input stream:
 
-{-| Forward downstream as many consecutive elements satisfying a predicate as
-    possible
+> printAll :: (Show a) = StateT (Producer a IO r) IO ()
+> printAll = run $ for input (liftIO . print)
+>
+> parser :: StateT (Producer Int IO r) IO ()
+> parser = do
+>     zoom (spans (< 4)) printAll  -- Limit to consecutive elements below 4
+>     liftIO $ putStrLn "Intermission"
+>     zoom (splits 3)    printAll  -- Limit to the next 3 elements
+
+>>> evalStateT parser (each [1..])
+1
+2
+3
+Intermission
+4
+5
+6
+
 -}
-passWhile :: (Monad m) => (a -> Bool) -> Draw -> Conduit a a (StateT [a] m) r
-passWhile pred = \_ -> go
+
+_span
+    :: (Monad m)
+    => (a -> Bool) -> Producer a m r -> Producer a m (Producer a m r)
+_span predicate = loop
   where
-    go = do
-        ma <- draw
-        case ma of
-            Nothing -> forever $ respond Nothing
-            Just a  ->
-                if (pred a)
+    loop p = do
+        x <- lift (next p)
+        case x of
+            Left   r      -> return (return r)
+            Right (e, p') ->
+                if (predicate e)
                 then do
-                    respond ma
-                    go
-                else do
-                    unDraw a
-                    forever $ respond Nothing
-{-# INLINABLE passWhile #-}
+                    yield e
+                    loop p'
+                else return (yield e >> p')
 
-{- $adapters
-    Use 'wrap' and 'unwrap' to convert between guarded and unguarded pipes.
+{-| Isomorphism between a 'Producer' and its 'span'
 
-    'fmapPull', 'returnPull', and 'bindPull' promote compatibility with
-    existing utilities that are not 'Maybe'-aware.
+    Use this isomorphism to limit a 'Producer' to the prefix of all elements
+    that satisfy a given predicate.
 -}
-
-{-| Guard a pipe from terminating by wrapping every output in 'Just' and ending
-    with a never-ending stream of 'Nothing's.
--}
-wrap
+spans
     :: (Monad m)
-    => (()   -> Proxy a' a ()          b  m r)
-    -> (Draw -> Proxy a' a Draw (Maybe b) m s)
-wrap f _ = do
-    (f ()) //> \b -> do
-        respond (Just b)
-        return ()
-    forever $ respond Nothing
-{-# INLINABLE wrap #-}
+    => (a -> Bool) -> Iso' (Producer a m r) (Producer a m (Producer a m r))
+spans predicate = L.iso (_span predicate) join
 
-{-| Compose 'unwrap' downstream of a guarded pipe to unwrap all 'Just's and
-    terminate on the first 'Nothing'.
+_splitAt :: (Monad m) => Int -> Producer a m r -> Producer a m (Producer a m r)
+_splitAt n p =
+    if (n <= 0)
+    then return p
+    else do
+        x <- lift $ next p
+        case x of
+            Left   r      -> return (return r)
+            Right (e, p') -> do
+                yield e
+                _splitAt (n - 1) p'
+
+
+{-| Isomorphism between a 'Producer' and its 'splitAt'
+
+    Use this isomorphism to limit a 'Producer' to a fixed number of elements.
 -}
-unwrap :: (Monad m) => () -> Proxy Draw (Maybe a) () a (StateT [a] m) ()
-unwrap () = go
-  where
-    go = do
-        ma <- draw
-        case ma of
-            Nothing -> return ()
-            Just a  -> do
-                respond a
-                go
-{-# INLINABLE unwrap #-}
+splits
+    :: (Monad m) => Int -> Iso' (Producer a m r) (Producer a m (Producer a m r))
+splits n = L.iso (_splitAt n) join
 
--- | Run a sub-parser with an isolated leftovers buffer
-using
-    :: (Monad m)
-    => [s]
-    -> Proxy a' a b' b (StateT [s] m) r
-    -> Proxy a' a b' b (StateT [s] m) r
-using s p = do
-    (r, s) <- hoist lift $ runStateP s p
-    lift $ S.modify (s ++)
-    return r
+{- $re-exports
+    @Control.Lens@ re-exports 'zoom' and 'Iso''.
 
-{-| Lift a 'Maybe'-oblivious pipe to a 'Maybe'-aware pipe by auto-forwarding
-    all 'Nothing's.
+    @Control.Monad.Trans.State@ re-exports 'StateT' (the type), 'runStateT',
+    'evalStateT', and 'execStateT'.
 
-> fmapPull f >-> fmapPull g = fmapPull (f >-> g)
->
-> fmapPull pull = pull
--}
-fmapPull
-    :: (Monad m)
-    => (()   -> Pipe    a b m r)
-    -> (Draw -> Conduit a b m r)
-fmapPull f = bindPull (f >-> returnPull)
-{-# INLINABLE fmapPull #-}
-
--- | Wrap all values flowing downstream in 'Just'.
-returnPull :: (Monad m) => Draw -> Proxy () a Draw (Maybe a) m r
-returnPull _ = forever $ do
-    a <- request ()
-    respond (Just a)
-{-# INLINABLE returnPull #-}
-
-{-| Lift a 'Maybe'-generating pipe to a 'Maybe'-transforming pipe by
-    auto-forwarding all 'Nothing's
-
-> -- Using: f >>> g = f >-> bindPull g
->
-> returnPull >>> f = f
->
-> f >>> returnPull = f
->
-> (f >>> g) >>> h = f >>> (g >>> h)
-
-Or equivalently:
-
-> returnPull >-> bindPull f = f
->
-> bindPull returnPull = pull
->
-> bindPull (f >-> bindPull g) = bindPull f >-> bindPull g
--}
-bindPull
-    :: (Monad m)
-    => (Draw -> Proxy () a Draw (Maybe b) m r)
-    -> (Draw -> Conduit  a             b  m r)
-bindPull f d = evalStateP d $ (up \>\ hoist lift . f />/ dn) d
-  where
-    up () = do
-        d  <- lift S.get
-        ma <- request d
-        case ma of
-            Nothing -> do
-                d2 <- respond Nothing
-                lift $ S.put d
-                up ()
-            Just a  -> return a
-    dn mb = do
-        d <- respond mb
-        lift $ S.put d
-        return d
-{-# INLINABLE bindPull #-}
-
--- | The 'Draw' type forces 
-data Draw = Draw
-
--- | Like a 'Consumer', but can only use 'draw' to request input
-type Sink    a   m r = forall y' y . Proxy Draw (Maybe a) y' y m r
-
--- | Like a 'Producer', except downstream pipes must use 'draw'
-type Source    b m r = forall x' x . Proxy x' x Draw (Maybe b) m r
-
--- | Like a 'Pipe', but may only 'draw' or transmit 'draw' requests
-type Conduit a b m r = Proxy Draw (Maybe a) Draw (Maybe b) m r
-
-{- $reexports
-
-    Control.Monad.Trans.State re-exports the 'StateT' type and all the run
-    functions.
+    @Control.Monad.IO.Class@ re-exports 'MonadIO'.
 -}
