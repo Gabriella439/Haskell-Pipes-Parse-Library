d85c22f381ae07b2a4781f223f5c6f64590be3e3
Gabriel Gonzalez <Gabriel439@gmail.com>
2014-01-25 19:48:45 +0700

    Removed dependency on `free`
    The `FreeT` functionality is split off into the `pipes-group` package

--- src/Pipes/Parse.hs
+++ src/Pipes/Parse.hs
@@ -26,42 +26,20 @@ module Pipes.Parse (
     groupBy,
     group,
 
-    -- * FreeT Lenses
-    groupsBy,
-    groups,
-    chunksOf,
-
-    -- * FreeT Transformations
-    takes,
-    takes',
-    drops,
-
-    -- * FreeT Joiners
-    concats,
-    intercalates,
-
-    -- * FreeT Folds
-    folds,
-    foldsM,
-
     -- * Re-exports
     -- $reexports
     module Control.Monad.Trans.Class,
-    module Control.Monad.Trans.Free,
     module Control.Monad.Trans.State.Strict,
     module Pipes
     ) where
 
 import Control.Monad (join)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Free (
-    FreeF(Pure, Free), FreeT(FreeT, runFreeT), transFreeT )
 import qualified Control.Monad.Trans.State.Strict as S
 import Control.Monad.Trans.State.Strict (
     StateT(StateT, runStateT), evalStateT, execStateT )
 import Data.Functor.Constant (Constant(Constant, getConstant))
 import Pipes (Producer, yield, next)
-import qualified Pipes as P
 
 import Prelude hiding (span, splitAt)
 
@@ -293,6 +271,9 @@ splitAt n0 k p0 = fmap join (k (to n0 p0))
                     to (n - 1) p'
 {-# INLINABLE splitAt #-}
 
+(^.) :: a -> ((b -> Constant b b) -> (a -> Constant b a)) -> b
+a ^. lens = getConstant (lens Constant a)
+
 {-| 'groupBy' is an improper lens from a 'Producer' to two 'Producer's split
     after the first group of elements grouped using the given equality predicate
 -}
@@ -316,227 +297,8 @@ group
 group = groupBy (==)
 {-# INLINABLE group #-}
 
-(^.) :: a -> ((b -> Constant b b) -> (a -> Constant b a)) -> b
-a ^. lens = getConstant (lens Constant a)
-
-{-| 'groupsBy' is an improper lens from a 'Producer' to a 'FreeT' of 'Producer's
-    grouped using the given equality predicate
--}
-groupsBy
-    :: Monad m
-    => (a -> a -> Bool) -> Lens' (Producer a m x) (FreeT (Producer a m) m x)
-groupsBy equals k p0 = fmap concats (k (to p0))
-  where
---  to :: Monad m => Producer a m r -> FreeT (Producer a m) m r
-    to p = FreeT $ do
-        x <- next p
-        return $ case x of
-            Left   r      -> Pure r
-            Right (a, p') -> Free $ fmap to ((yield a >> p')^.span (equals a))
-{-# INLINABLE groupsBy #-}
-
--- | Like 'groupsBy', where the equality predicate is ('==')
-groups :: (Monad m, Eq a) => Lens' (Producer a m x) (FreeT (Producer a m) m x)
-groups = groupsBy (==)
-{-# INLINABLE groups #-}
-
-{-| 'chunksOf' is an improper lens from a 'Producer' to a 'FreeT' of 'Producer's
-    of fixed length
--}
-chunksOf
-    :: Monad m => Int -> Lens' (Producer a m x) (FreeT (Producer a m) m x)
-chunksOf n0 k p0 = fmap concats (k (to p0))
-  where
---  to :: Monad m => Producer a m x -> FreeT (Producer a m) m x
-    to p = FreeT $ do
-        x <- next p
-        return $ case x of
-            Left   r      -> Pure r
-            Right (a, p') -> Free $ do
-                p'' <- (yield a >> p')^.splitAt n0
-                return (to p'')
-{-# INLINABLE chunksOf #-}
-
--- | Join a 'FreeT'-delimited stream of 'Producer's into a single 'Producer'
-concats :: Monad m => FreeT (Producer a m) m x -> Producer a m x
-concats = go
-  where
-    go f = do
-        x <- lift (runFreeT f)
-        case x of
-            Pure r -> return r
-            Free p -> do
-                f' <- p
-                go f'
-{-# INLINABLE concats #-}
-
-{-| Join a 'FreeT'-delimited stream of 'Producer's into a single 'Producer' by
-    intercalating a 'Producer' in between them
--}
-intercalates
-    :: Monad m => Producer a m () -> FreeT (Producer a m) m x -> Producer a m x
-intercalates sep = go0
-  where
-    go0 f = do
-        x <- lift (runFreeT f)
-        case x of
-            Pure r -> return r
-            Free p -> do
-                f' <- p
-                go1 f'
-    go1 f = do
-        x <- lift (runFreeT f)
-        case x of
-            Pure r -> return r
-            Free p -> do
-                sep
-                f' <- p
-                go1 f'
-{-# INLINABLE intercalates #-}
-
--- | @(takes n)@ only keeps the first @n@ functor layers of a 'FreeT'
-takes :: (Functor f, Monad m) => Int -> FreeT f m () -> FreeT f m ()
-takes = go
-  where
-    go n f =
-        if (n > 0)
-        then FreeT $ do
-            x <- runFreeT f
-            case x of
-                Pure () -> return (Pure ())
-                Free w  -> return (Free (fmap (go $! n - 1) w))
-        else return ()
-{-# INLINABLE takes #-}
-
-{-| @(takes' n)@ only keeps the first @n@ 'Producer's of a 'FreeT'
-
-    'takes'' differs from 'takes' by draining unused 'Producer's in order
-    to preserve the return value.  This makes it a suitable argument for
-    'transFreeT'
--}
-takes'
-    :: Monad m => Int -> FreeT (Producer a m) m x -> FreeT (Producer a m) m x
-takes' = go0
-  where
-    go0 n f = FreeT $
-        if (n > 0)
-        then do
-            x <- runFreeT f
-            return $ case x of
-                Pure r -> Pure r
-                Free p -> Free $ fmap (go0 $! n - 1) p
-        else go1 f
-    go1 f = do
-        x <- runFreeT f
-        case x of
-            Pure r -> return (Pure r)
-            Free p -> do
-                f' <- P.runEffect (P.for p P.discard)
-                go1 f'
-{-# INLINABLE takes' #-}
-
-{-| @(drops n)@ peels off the first @n@ 'Producer' layers of a 'FreeT'
-
-    Use carefully: the peeling off is not free.   This runs the first @n@
-    layers, just discarding everything they produce.
--}
-drops
-    :: Monad m => Int -> FreeT (Producer a m) m x -> FreeT (Producer a m) m x
-drops = go
-  where
-    go n ft
-        | n <= 0 = ft
-        | otherwise = FreeT $ do
-            ff <- runFreeT ft
-            case ff of
-                Pure _ -> return ff
-                Free f -> do
-                    ft' <- P.runEffect $ P.for f P.discard
-                    runFreeT $ go (n-1) ft'
-{-# INLINABLE drops #-}
-
-{-| Fold each 'Producer' of a 'FreeT'
-
-@
- 'Control.Foldl.purely' folds
-     :: Monad m => 'Control.Foldl.Fold' a b -> FreeT (Producer a m) m r -> Producer b m r
-@
--}
-folds
-    :: Monad m
-    => (x -> a -> x)
-    -- ^ Step function
-    -> x
-    -- ^ Initial accumulator
-    -> (x -> b)
-    -- ^ Extraction function
-    -> FreeT (Producer a m) m r
-    -- ^
-    -> Producer b m r
-folds step begin done = go
-  where
-    go f = do
-        x <- lift (runFreeT f)
-        case x of
-            Pure r -> return r
-            Free p -> do
-	        (f', b) <- lift (fold p begin)
-	        yield b
-	        go f'
-
-    fold p x = do
-        y <- next p
-        case y of
-            Left   f      -> return (f, done x)
-            Right (a, p') -> fold p' $! step x a
-{-# INLINABLE folds #-}
-
-{-| Fold each 'Producer' of a 'FreeT', monadically
-
-@
- 'Control.Foldl.impurely' foldsM
-     :: Monad m => 'Control.Foldl.FoldM' a b -> FreeT (Producer a m) m r -> Producer b m r
-@
--}
-foldsM
-    :: Monad m
-    => (x -> a -> m x)
-    -- ^ Step function
-    -> m x
-    -- ^ Initial accumulator
-    -> (x -> m b)
-    -- ^ Extraction function
-    -> FreeT (Producer a m) m r
-    -- ^
-    -> Producer b m r
-foldsM step begin done = go
-  where
-    go f = do
-        y <- lift (runFreeT f)
-        case y of
-            Pure r -> return r
-            Free p -> do
-                (f', b) <- lift $ do
-                    x <- begin
-		    foldM p x
-                yield b
-                go f'
-
-    foldM p x = do
-        y <- next p
-        case y of
-            Left   f      -> do
-                b <- done x
-                return (f, b)
-            Right (a, p') -> do
-                x' <- step x a
-                foldM p' $! x'
-
 {- $reexports
     "Control.Monad.Trans.Class" re-exports 'lift'.
-
-    "Control.Monad.Trans.Free" re-exports 'FreeF', 'FreeT', 'runFreeT', and
-    'transFreeT'.
 
     "Control.Monad.Trans.State.Strict" re-exports 'StateT', 'runStateT',
     'evalStateT', and 'execStateT'.
